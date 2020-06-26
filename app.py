8cd521ea3c6a017f5ace4d96f444c3f9e0712143
Neisvestney <neisvestney113@gmail.com>
2020-06-26 19:04:00 +0300

    Add favicon

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import os
+
+from flask import Flask, send_from_directory
 from flask import render_template as render
 
 app = Flask(__name__)
@@ -18,9 +20,16 @@ def game():
 def cheat():
     return render('cheat.html')
 
+
 @app.route('/purchases')
 def hello_world3():
     return render('purchases.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
     app.run(debug=True)

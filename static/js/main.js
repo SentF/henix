0a9444e94fdab5575bfee98dfda0db96e273dd6a
Neisvestney <neisvestney113@gmail.com>
2020-06-26 18:23:10 +0300

    Add auto copy

--- static/js/main.js
+++ static/js/main.js
@@ -2,12 +2,12 @@ $(document).ready(function () {
     $('#btn_key').click(function () {
         Swal.fire({
             html:
-                '<p style="font-size: 14px; color: #FF5C00; margin-bottom: 8px;">Thank you for buying</p>'+
-                '<p style="color: #fff"> Get the key to activate the hack.</p>'+
+                '<p style="font-size: 14px; color: #FF5C00; margin-bottom: 8px;">Thank you for buying</p>' +
+                '<p style="color: #fff"> Get the key to activate the hack.</p>' +
                 '<div class="fake_input">' +
-                    '<text style="text-align: left; font-size: 16px; margin-left: 8px" class="blue_text">H12TJI21F2130JI0TDSSPAD412</text>' +
-                    '<button onclick="copy()" class="copy_icon"></button>'+
-                '</div>'+
+                '<text style="text-align: left; font-size: 16px; margin-left: 8px" class="blue_text">H12TJI21F2130JI0TDSSPAD412</text>' +
+                '<button onclick="copy()" class="copy_icon"></button>' +
+                '</div>' +
                 '<button onclick="closeSwal()" style="margin-top: 16px; width: 286px; height: 42px; background: #FF5C00; color: #fff; border-radius: 5px; border: #FF5C00">Done</button>'
             ,
             showConfirmButton: false,
@@ -21,7 +21,17 @@ $(document).ready(function () {
 });
 
 function copy() {
-
+    if ($('#tmp').length) {
+        $('#tmp').remove();
+    }
+    var clickText = $(this).text();
+    $('<textarea id="tmp" />')
+        .appendTo($('.swal2-container'))
+        .val($('.swal2-container text').text())
+        .focus()
+        .select();
+    document.execCommand("copy");
+    $('#tmp').remove()
 }
 
 function closeSwal() {

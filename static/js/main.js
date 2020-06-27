c4702ba2e4e3b0c97b6339ec3bcd847f143773a9
Xefise <Xefise@gmail.com>
2020-06-27 15:45:37 +0300

    Fix swal

--- static/js/main.js
+++ static/js/main.js
@@ -4,30 +4,23 @@ $(document).ready(function () {
             html:
                 `<p class="title_swal">Thank you for buying</p>
                 <p class="text_swal"> Get the key to activate the hack.</p>
-                <div class="fake_input">
-                <text style="text-align: left; font-size: 16px; margin-left: 8px" class="blue_text">H12TJI21F2130JI0TDSSPAD412</text>
-                <button onclick="copy()" class="copy_icon"></button>
+                <div class="fake_input btn-block">
+                    <text style="text-align: left; font-size: 16px; margin-left: 8px" class="blue_text">H12TJI21F2130JI0TDSSPAD412</text>
+                    <button onclick="copy()" class="copy_icon"></button>
                 </div>
-                <button onclick="closeSwal()" class="confirm_swal">Done</button>`
+                <button onclick="closeSwal()" class="confirm_swal btn-block">Done</button>`
             ,
             showConfirmButton: false,
             showCloseButton: false,
             showCancelButton: false,
             focusConfirm: false,
             width: 331,
+            padding: "28px",
             background: '#1F2346',
         })
+        $('.swal2-show').addClass("swal_updater");
     });
 
-    let setPadding
-    if(window.screen.availWidth > 450) {
-        setPadding = "20px";
-        setWidth = "424px";
-    }
-    else {
-        setPadding = "10px";
-        setWidth = "300px";
-    }
     $('#btn_buy').click(function () {
         Swal.fire({
             html: `<p class="title_swal">Payment</p>
@@ -35,7 +28,7 @@ $(document).ready(function () {
                 <div class="payment_methods"> 
                     <div>
                         <input type="radio" id="bitcoin" value="bitcoin" name="payment_methods"/>
-                        <label class="payment_method bitcoin_method" for="bitcoin" ></label>
+                        <label  class="payment_method bitcoin_method" for="bitcoin" ></label>
                     </div>
                     <div>
                         <input type="radio" id="paypal" value="paypal" name="payment_methods" />
@@ -43,17 +36,17 @@ $(document).ready(function () {
                     </div>
                     <div>
                         <input type="radio" id="credit_card" value="credit_card" name="payment_methods" />
-                        <label class="payment_method credit_card_method" for="credit_card" ></label>
+                        <label style="" class="payment_method credit_card_method" for="credit_card" ></label>
                     </div>
                 </div>
-                <input type="email" class="form-control input_swal" id="exampleFormControlInput1" placeholder="name@example.com">
-                <button onclick="pay()" class="confirm_swal">Pay</button>`
+                <input type="email" class="form-control input_swal btn-block" id="exampleFormControlInput1" placeholder="name@example.com">
+                <button onclick="pay()" class="confirm_swal btn-block">Pay</button>`
             ,
             showConfirmButton: false,
             showCloseButton: false,
             showCancelButton: false,
             focusConfirm: false,
-            padding: "20px",
+            padding: "28px",
             width: 424,
             background: '#1F2346',
         });

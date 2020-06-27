389b37d733f4580381c5662c1e94dfc1786f9878
Xefise <Xefise@gmail.com>
2020-06-27 15:00:38 +0300

    Add payment

--- static/js/main.js
+++ static/js/main.js
@@ -2,13 +2,13 @@ $(document).ready(function () {
     $('#btn_key').click(function () {
         Swal.fire({
             html:
-                '<p class="title_swal">Thank you for buying</p>'+
-                '<p class="text_swal"> Get the key to activate the hack.</p>'+
-                '<div class="fake_input">' +
-                    '<text style="text-align: left; font-size: 16px; margin-left: 8px" class="blue_text">H12TJI21F2130JI0TDSSPAD412</text>' +
-                    '<button onclick="copy()" class="copy_icon"></button>'+
-                '</div>'+
-                '<button onclick="closeSwal()" class="confirm_swal">Done</button>'
+                `<p class="title_swal">Thank you for buying</p>
+                <p class="text_swal"> Get the key to activate the hack.</p>
+                <div class="fake_input">
+                <text style="text-align: left; font-size: 16px; margin-left: 8px" class="blue_text">H12TJI21F2130JI0TDSSPAD412</text>
+                <button onclick="copy()" class="copy_icon"></button>
+                </div>
+                <button onclick="closeSwal()" class="confirm_swal">Done</button>`
             ,
             showConfirmButton: false,
             showCloseButton: false,
@@ -19,35 +19,45 @@ $(document).ready(function () {
         })
     });
 
+    let setPadding
+    if(window.screen.availWidth > 450) {
+        setPadding = "20px";
+        setWidth = "424px";
+    }
+    else {
+        setPadding = "10px";
+        setWidth = "300px";
+    }
     $('#btn_buy').click(function () {
         Swal.fire({
-            html:
-                '<p class="title_swal">Payment</p>'+
-                '<p class="text_swal"> Select payment method.</p>'+
-                '<div class="payment_methods"> '+
-                    '<input type="radio" name="payment_methods" class="payment_methods ">'+
-                        '<p class="bitcoin_icon>"></p> '+
-                        '<p class="white_text">Bitcoin</p>'+
-                    '</input>'+
-                    '<input type="radio" name="payment_methods" class="payment_methods ">'+
-                        '<p class="paypal_icon>"></p> '+
-                        '<p class="white_text">PayPal</p>'+
-                    '</input>'+
-                    '<input type="radio" name="payment_methods" class="payment_methods ">'+
-                        '<p class="credit_card_icon>"></p> '+
-                        '<p class="white_text">Credit card</p>'+
-                    '</input>'+
-                '</div>'+
-                '<input type="email" class="form-control input_swal" id="exampleFormControlInput1" placeholder="name@example.com">'+
-                '<button onclick="pay()" class="confirm_swal">Pay</button>'
+            html: `<p class="title_swal">Payment</p>
+                <p style="margin-bottom: 0" class="text_swal"> Select payment method.</p>
+                <div class="payment_methods"> 
+                    <div>
+                        <input type="radio" id="bitcoin" value="bitcoin" name="payment_methods"/>
+                        <label class="payment_method bitcoin_method" for="bitcoin" ></label>
+                    </div>
+                    <div>
+                        <input type="radio" id="paypal" value="paypal" name="payment_methods" />
+                        <label class="payment_method paypal_method" for="paypal" ></label>
+                    </div>
+                    <div>
+                        <input type="radio" id="credit_card" value="credit_card" name="payment_methods" />
+                        <label class="payment_method credit_card_method" for="credit_card" ></label>
+                    </div>
+                </div>
+                <input type="email" class="form-control input_swal" id="exampleFormControlInput1" placeholder="name@example.com">
+                <button onclick="pay()" class="confirm_swal">Pay</button>`
             ,
             showConfirmButton: false,
             showCloseButton: false,
             showCancelButton: false,
             focusConfirm: false,
-            width: 331,
+            padding: "20px",
+            width: 424,
             background: '#1F2346',
-        })
+        });
+        $('.swal2-show').addClass("swal_updater");
     });
 });
 

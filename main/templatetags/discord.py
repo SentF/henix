1f7f10251d59d4d5fca075eab04964015f028281
Neisvestney <neisvestney113@gmail.com>
2020-07-01 15:34:14 +0300

    Fix bug with viewing pages without socialaccount

--- main/templatetags/discord.py
+++ main/templatetags/discord.py
@@ -1,12 +1,16 @@
-import json
-
 from allauth.socialaccount.models import SocialAccount
 from django import template
+from django.core.exceptions import ObjectDoesNotExist
+from django.templatetags.static import static
 
 register = template.Library()
 
 
 @register.filter(name='get_avatar')
 def get_avatar(value):
-    data = SocialAccount.objects.get(user_id=value).extra_data
-    return f'https://cdn.discordapp.com/avatars/{data["id"]}/{data["avatar"]}'
+    try:
+        data = SocialAccount.objects.get(user_id=value).extra_data
+        return f'https://cdn.discordapp.com/avatars/{data["id"]}/{data["avatar"]}'
+    except ObjectDoesNotExist:
+        return static('images/profile.png')
+

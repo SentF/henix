1005a2e48e5fca29c9871b4db6dedb039b371593
Xefise <45072628+Xefise@users.noreply.github.com>
2020-07-06 21:04:00 +0300

    Update DB for DS (#3)
    Co-authored-by: Neisvestney <neisvestney113@gmail.com>

---
+++ unitpay/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from main.views.cheat import cheat
+from main.views.game import game
+from main.views.index import index
+from main.views.purchases import purchases
+
+urlpatterns = [
+
+]

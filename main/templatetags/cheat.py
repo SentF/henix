7a7e19920153d1aaf88b2d29f0b64d8e587ed677
Neisvestney <neisvestney113@gmail.com>
2020-07-09 19:14:40 +0300

    -kostil (cheat.html|hack func)

--- main/templatetags/cheat.py
+++ main/templatetags/cheat.py
@@ -24,4 +24,4 @@ def functions(value, split):
 
 @register.filter(name='quantity')
 def quantity(value):
-    return Cheat.objects.get(id=value).key_set.filter(is_sold=False).count()
+    return Cheat.objects.get(id=value).key_set.filter(purchase=None).count()

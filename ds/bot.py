9d05686e51d5708427ed4095902320258df60160
Xefise <Xefise@gmail.com>
2020-07-11 23:05:55 +0300

    Melik TY :b

--- ds/bot.py
+++ ds/bot.py
@@ -1,62 +1,46 @@
-import asyncio
 import os
 import threading
+from functools import partial
+
 import discord
+from discord.ext import commands
 from allauth.socialaccount.models import SocialAccount
-from asgiref.sync import sync_to_async
-from channels.db import database_sync_to_async
 
-from main.models import Game, Purchase, Key
+from main.models import Game, Key
 
+bot = commands.Bot("_")
 
 def start():
     thread = threading.Timer(0, ds)
     thread.daemon = True
     thread.start()
 
-
 def ds():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    client = discord.Client()
-
-    @client.event
+    @bot.event
     async def on_ready():
-        print('We have logged in as {0.user}'.format(client))
+        print('We have logged in as {0.user}'.format(bot))
 
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
 
-        if message.content.startswith('$hello'):
-            game = await sync_to_async(lambda: Game.objects.all()[0], thread_sensitive=True)()
-            await message.channel.send(game.name)
+    @commands.command()
+    async def hello(ctx):
+        game = (await bot.loop.run_in_executor(None, Game.objects.all))[0]
+        await ctx.send(game.name)
 
-    @client.event
+
+    @bot.event
     async def on_member_join(member):
-        keys = await get_member_keys(member)
+        allAccounts = await bot.loop.run_in_executor(None, SocialAccount.objects.all)
+        for acc in allAccounts:
+            if acc.extra_data['id'] == str(member.id):
+                account_id = acc.extra_data['id']
+                purchases = await bot.loop.run_in_executor(None, partial(Key.objects.filter, user__id=account_id))
+                keys = [await bot.loop.run_in_executor(None, partial(Key.objects.filter, purchase__id=purchase.id)) for
+                        purchase in purchases]
         for key in keys:
-            role = member.guild.get_role(key.cheat.ds_role_id)
+            role = discord.utils.get(member.guild.roles, id=key.cheat.ds_role_id)
             print(str(key.cheat.ds_role_id))
-            if role != None: await member.add_roles(role)
+            await member.add_roles(role)
+            break
 
 
-    @client.event
-    async def on_error(event, *args, **kwargs):
-        client.run(os.environ.get('DS_BOT_TOKEN'))
-
-    client.run(os.environ.get('DS_BOT_TOKEN'))
-
-
-async def get_member_keys(member):
-    allAccounts = await sync_to_async(lambda: SocialAccount.objects.all(), thread_sensitive=True)()
-    for acc in allAccounts:
-        if acc.extra_data['id'] == str(member.id):
-            account_id = acc.extra_data['id']
-            purchases = await database_sync_to_async(lambda: Purchase.objects.all(), thread_sensitive=True)()
-            purchases = list(filter(lambda p: str(p.user.socialaccount_set.all()[0].uid) == account_id, purchases))
-            keys = [await database_sync_to_async(lambda: Key.objects.all()[0], thread_sensitive=True)() for purchase in purchases]
-            return keys
-        break
-    return []
+    bot.run(os.environ.get('DS_BOT_TOKEN'))
